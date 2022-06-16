a1c1e6e343278448b663a53f6f5f189a03f4053b
EnigmaZhang <zhangxiaotiannew@gmail.com>
2022-06-16 15:33:09 +0800

    12.19

--- Str_Blob.h
+++ Str_Blob.h
@@ -6,6 +6,8 @@
 #include <memory>
 #include <stdexcept>
 
+class StrBlobPtr;
+
 class StrBlob
 {
 public:
@@ -17,6 +19,11 @@ public:
 	void pop_back();
 	std::string& front() const;
 	std::string& back() const;
+
+	friend class StrBlobPtr;
+	StrBlobPtr begin() { return StrBlobPtr(*this); };
+	StrBlobPtr end() { return StrBlobPtr(*this, data->size()); };
+
 private:
 	std::shared_ptr<std::vector<std::string>> data;
 	void check(size_type i, const std::string& msg) const;
@@ -50,6 +57,46 @@ void StrBlob::pop_back()
 {
 	check(0, "pop_back on empty StrBlob");
 	data->pop_back();
+}
+
+class StrBlobPtr
+{
+public:
+	StrBlobPtr() :curr(0) {}
+	StrBlobPtr(StrBlob& a, size_t sz = 0) : wptr(a.data), curr(sz) {}
+	std::string& deref() const;
+	StrBlobPtr& incr();
+private:
+	std::shared_ptr<std::vector<std::string>> check(std::size_t, const std::string&) const;
+	std::weak_ptr<std::vector<std::string>> wptr;
+	std::size_t curr;
+};
+
+std::shared_ptr<std::vector<std::string>> StrBlobPtr::check(std::size_t i, const std::string& msg) const
+{
+	auto ret{ wptr.lock() };
+	if (!ret)
+	{
+		throw std::runtime_error("unbound StrBlobPtr");
+	}
+	if (i >= ret->size())
+	{
+		throw std::out_of_range(msg);
+	}
+	return ret;
+}
+
+std::string& StrBlobPtr::deref() const
+{
+	auto p{ check(curr, "dereference past end") };
+	return (*p)[curr];
+}
+
+StrBlobPtr& StrBlobPtr::incr()
+{
+	check(curr, "increment past end of StrBlobStr");
+	++curr;
+	return *this;
 }
 
 #endif // !STR_BLOB_H
