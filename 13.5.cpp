f7859b255e6ad9cc9d89f35f6a2885f793a7ee4c
EnigmaZhang <zhangxiaotiannew@gmail.com>
2023-01-12 11:34:05 +0800

    13.5

---
+++ 13.5.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+
+class HasPtr 
+{
+public:
+	HasPtr(const std::string& s = std::string()) :
+		ps(new std::string(s)), i(0) {}
+	HasPtr(const HasPtr& origin) 
+	{
+		ps = new std::string(*origin.ps);
+		i = origin.i;
+	}
+	
+	std::string* get_ps() { return ps; }
+	int get_i() { return i; }
+private:
+	std::string* ps;
+	int i;
+};
+
+int main()
+{
+	HasPtr p1 { HasPtr(std::string("abc")) };
+	HasPtr p2{ p1 };
+	std::cout << (p1.get_ps() == p2.get_ps()) << std::endl;
+	std::cout << (*p1.get_ps() == *p2.get_ps()) << std::endl;
+	std::cout << (p1.get_i() == p2.get_i()) << std::endl;
+}
