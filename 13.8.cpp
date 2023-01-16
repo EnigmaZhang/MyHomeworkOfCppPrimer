0a5bea3a6ecb948f5facec99dff252d8ca72d3e3
EnigmaZhang <zhangxiaotiannew@gmail.com>
2023-01-16 15:57:47 +0800

    add 13.8

--- 13.8.cpp
+++ 13.8.cpp
@@ -6,10 +6,19 @@ class HasPtr
 public:
 	HasPtr(const std::string& s = std::string()) :
 		ps(new std::string(s)), i(0) {}
+
 	HasPtr(const HasPtr& origin) 
 	{
 		ps = new std::string(*origin.ps);
 		i = origin.i;
+	}
+
+	HasPtr& operator=(const HasPtr& origin)
+	{
+		this->ps = new std::string(*origin.ps);
+		i = origin.i;
+
+		return *this;
 	}
 	
 	std::string* get_ps() { return ps; }
@@ -22,10 +31,11 @@ private:
 int main()
 {
 	HasPtr p1 { HasPtr(std::string("abc")) };
-	HasPtr p2{ p1 };
+	HasPtr p2;
+	p2 = p1;
 	std::cout << (p1.get_ps() == p2.get_ps()) << std::endl;
 	std::cout << (*p1.get_ps() == *p2.get_ps()) << std::endl;
 	std::cout << (p1.get_i() == p2.get_i()) << std::endl;
-
-	return 0;
+    
+    return 0;
 }
